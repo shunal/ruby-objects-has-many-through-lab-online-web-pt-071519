9a39459ba36301398d64760471cf9f081cd2e29c
Steven Hunal <stevehunal@gmail.com>
2019-08-09 03:19:38 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -17,4 +17,15 @@ class Doctor
     Appointment.new(date, patient, self)
   end 
   
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.doctor == self
+    end
+  end 
+  
+  def patients 
+    appointments.collect do |appointment|
+      appointment.patient
+  end 
+  
 end 
