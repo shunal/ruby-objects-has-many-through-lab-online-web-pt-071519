e4327ea6c64dd3b756ebc2d5408e8f285e8a9619
Steven Hunal <stevehunal@gmail.com>
2019-08-09 02:35:55 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,13 +2,15 @@ class Artist
   
   attr_accessor :name 
   
-  @all = []
+  @@all = []
   
   def initialize(name)
     @name = name
     @@all << self
   end 
   
-  
+  def self.all 
+    @@all
+  end 
   
 end 
